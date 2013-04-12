2ddad01055e8228206e95cf005649e556f16d254
stroupaloop <andrew.m.stroup@gmail.com>
2013-04-11 20:39:48 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stroupaloop::Application.config.secret_token = '331086605382a51412fc97ced338bdc40b3aa8c10cff8f4acfbb17497c1413c442c79c2a054fda8d04b0479f05045df919591d4d42829bd1a94fc89d53844d39'
